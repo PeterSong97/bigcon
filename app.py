e61158094e3d71a37d8dc0f1ba53df76eaea8894
PeterSong97 <129484762+PeterSong97@users.noreply.github.com>
2024-10-23 14:02:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -175,15 +175,11 @@ def load_faiss_index(index_path):
 index = load_faiss_index(index_path)
 
 # 텍스트 임베딩 함수
-def embed_text(texts, batch_size=8):
-    all_embeddings = []
-    for i in range(0, len(texts), batch_size):
-        batch_texts = texts[i:i+batch_size]
-        inputs = tokenizer(batch_texts, return_tensors='pt', padding=True, truncation=True).to(device)
-        with torch.no_grad():
-            embeddings = embedding_model(**inputs).last_hidden_state.mean(dim=1)
-        all_embeddings.append(embeddings.cpu().numpy())  # 임베딩을 리스트에 추가
-    return np.vstack(all_embeddings)
+def embed_text(text):
+    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True).to(device)
+    with torch.no_grad():
+        embeddings = embedding_model(**inputs).last_hidden_state.mean(dim=1)
+    return embeddings.squeeze().cpu().numpy()
 
 # 임베딩 로드
 embeddings = np.load(os.path.join(module_path, 'embeddings_array_file.npy'))
