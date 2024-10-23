1a2cd9cc9f59c93829ed808d3d6aa510f3783cff
PeterSong97 <129484762+PeterSong97@users.noreply.github.com>
2024-10-23 13:07:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import time
 # import base64
 from datetime import datetime
 # import aiohttp
-import asyncio
+# import asyncio
 
 # 가장 먼저 set_page_config 호출
 st.set_page_config(page_title="🥙🌮🥯제주 맛집 찾아 삼만리🥯🌮🥙")
